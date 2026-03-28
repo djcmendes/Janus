a19bb443b23437176b03b308aeb9d89fc833aea5
David Mendes <djcmendes@gmail.com>
2026-03-28 12:02:41 +0000

    feat(portals): add PublishPageCommand and UnpublishPageCommand with handlers [#032]

---
+++ backend.janus.com/src/Portals/Application/Command/Handler/UnpublishPageHandler.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+namespace App\Portals\Application\Command\Handler;
+use App\Portals\Application\Command\UnpublishPageCommand;
+use App\Portals\Application\DTO\PageDto;
+use App\Portals\Domain\Exception\PageNotFoundException;
+use App\Portals\Domain\Repository\PageRepositoryInterface;
+final class UnpublishPageHandler
+{
+    public function __construct(private readonly PageRepositoryInterface $repository) {}
+    public function handle(UnpublishPageCommand $command): PageDto
+    {
+        $page = $this->repository->findById($command->id);
+        if ($page === null) { throw new PageNotFoundException($command->id); }
+        $page->unpublish();
+        $this->repository->save($page);
+        return PageDto::fromEntity($page);
+    }
+}
---
+++ backend.janus.com/src/Portals/Application/Command/PublishPageCommand.php
@@ -0,0 +1,7 @@
+<?php
+declare(strict_types=1);
+namespace App\Portals\Application\Command;
+final class PublishPageCommand
+{
+    public function __construct(public readonly string $id) {}
+}
